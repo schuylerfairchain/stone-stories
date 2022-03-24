1843b4b2449720f70ca0dc2a4fdf9b7288ffd763
Maciej Ziarkowski <maciej.ziarkowski@gmail.com>
2022-03-24 18:00:47 -0400

    Add floor, grabbable cube text

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import { Box } from '@react-three/drei';
-import { Hands, VRCanvas } from '@react-three/xr';
+import { Box, Plane } from '@react-three/drei';
+import { VRCanvas } from '@react-three/xr';
 
-import { Rotating } from './Rotating';
 import './App.css';
+import { DefaultHandControllers } from './lib/react-xr-default-hands/DefaultHandControllers';
+import { Grabbable } from './lib/react-xr-default-hands/Grabbable';
+
+function Floor() {
+  return <Plane args={[5,5]} rotation={[-Math.PI/2, 0, 0]} position={[0, -1, 0]}>
+    <meshBasicMaterial color="gray" />
+    </Plane>;
+}
 
 function App() {
   return (
     <div className="App">
       <main className="App-body">
         <VRCanvas id="vr">
-          <Hands />
-
+          <DefaultHandControllers />
+          
+          <Floor />
           <ambientLight name="main-ambient-light" intensity={0.3} />
           <pointLight name="main-point-light" intensity={1.5} position={[-5, -2, -2]} />
-          <group position={[0,1,-5]} scale={[0.2, 0.2, 0.2]}>
-            <Rotating>
-                <Box name="cube" scale={[2,2,2]}>
-                  <meshStandardMaterial attach="material" color='red' />
-                </Box>
-            </Rotating>
-          </group>
+
+          <Grabbable position={[0,2,-0.25]}>
+              <Box args={[0.2, 0.2, 0.2]} name="cube">
+                <meshStandardMaterial attach="material" color='red' />
+              </Box>
+          </Grabbable>
         </VRCanvas>
       </main>
     </div>
