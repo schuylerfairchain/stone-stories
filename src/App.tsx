7abff256493e4c0921f149588be4d8875cb9c541
Maciej Ziarkowski <maciej.ziarkowski@gmail.com>
2022-03-25 12:19:49 -0400

    Add shadow casting

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,14 @@ import { Grabbable } from './Grabbable';
 function Floor({...props}) {
   const [ref] = usePlane(() => ({ ...props }));
 
-  return <Plane ref={ref} args={[10,10]}>
-    <meshBasicMaterial color="gray" />
+  return <Plane ref={ref} args={[10,10]} receiveShadow>
+    <meshPhongMaterial color="gray" />
     </Plane>;
 }
 
 function TestCube() {
-  return <Box args={[0.2, 0.2, 0.2]} name="cube">
-    <meshStandardMaterial attach="material" color='red' />
+  return <Box castShadow args={[0.2, 0.2, 0.2]} name="cube">
+    <meshPhongMaterial color='red'/>
   </Box>;
 }
 
@@ -24,7 +24,7 @@ function World() {
 
   return (
     <Physics>
-        <Floor rotation={[-Math.PI / 2, 0, 0]} position={[0, -2, 0]} />
+        <Floor rotation={[-Math.PI / 2, 0, 0]} position={[0, -1.5, 0]} />
 
       <Grabbable position={[0,2,-0.25]}>
         <TestCube />
@@ -36,10 +36,10 @@ function App() {
   return (
     <div className="App">
       <main className="App-body">
-        <VRCanvas id="vr">
+        <VRCanvas id="vr" shadows>
           <DefaultHandControllers />
           <ambientLight name="main-ambient-light" intensity={0.3} />
-          <pointLight name="main-point-light" intensity={1.5} position={[-5, -2, -2]} />
+          <pointLight name="main-point-light" intensity={1.5} position={[0, 3, -2]} castShadow/>
           
           <World />
         </VRCanvas>
