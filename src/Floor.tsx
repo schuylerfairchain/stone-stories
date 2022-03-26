6af41500f0eb10100b1476ff1bcdbcdf5f3395bd
Schuyler deVos <schuylerdevos@gmail.com>
2022-03-26 13:23:33 -0400

    new plane

--- src/Floor.tsx
+++ src/Floor.tsx
@@ -1,14 +1,15 @@
 import { usePlane } from "@react-three/cannon";
 import { Plane } from "@react-three/drei";
+import { useGLTF } from "@react-three/drei";
 
-export function Floor({invisible, ...props}) {
-    const [ref] = usePlane(() => ({ ...props, type: 'Static' }));
-  
-    return <Plane ref={ref} args={[10,10]} receiveShadow>
-      {
-        invisible ?
-        <shadowMaterial /> :
-      <meshPhongMaterial color="gray" />
-      }
-      </Plane>;
-  }
+useGLTF.preload(process.env.PUBLIC_URL + "/models/CirclePlane.gltf");
+
+export function Floor({ invisible, ...props }) {
+  const [ref] = usePlane(() => ({ ...props, type: "Static" }));
+
+  return (
+    <Plane ref={ref} args={[10, 10]} receiveShadow>
+      {invisible ? <shadowMaterial /> : <meshPhongMaterial color="gray" />}
+    </Plane>
+  );
+}
